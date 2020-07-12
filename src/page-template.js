e99bcea6c5dc56f727c0b52feb5fd135d044af68
Brandon Jackson <brandonljackson23@gmail.com>
2020-07-12 14:58:26 -0600

    optimize code  in page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -1,54 +1,29 @@
 const generateTeam = (team) => {
     let employees = '';
-
     for (i = 0; i < team.length; i++) {
-        if (team[i].role === 'Manager') {
-            employees += `
-            <div class="card employee-card border-primary shadow">
-                <div class="card-header text-center text-white bg-primary">
-                    <h2 class="card-title">${team[i].name}</h2>
-                    <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${team[i].role}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${team[i].id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
-                        <li class="list-group-item">Office number: ${team[i].officeNumber}</li>
-                    </ul>
-                </div>
-            </div>`
+        let uniqueInfo = '';
+        if (team[i].role === 'Manager') { 
+            uniqueInfo = `<li class="list-group-item">Office number: ${team[i].officeNumber}</li>`
         } else if (team[i].role === 'Engineer') {
-            employees += `
-            <div class="card employee-card border-primary shadow">
-                <div class="card-header text-center text-white bg-primary">
-                    <h2 class="card-title">${team[i].name}</h2>
-                    <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${team[i].role}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${team[i].id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
-                        <li class="list-group-item">GitHub: <a href="https://github.com/${team[i].github}" target="_blank" rel="noopener noreferrer">${team[i].github}</a></li>
-                    </ul>
-                </div>
-            </div>`
+            uniqueInfo = `<li class="list-group-item">GitHub: <a href="https://github.com/${team[i].github}" target="_blank" rel="noopener noreferrer">${team[i].github}</a></li>`
         } else if (team[i].role === 'Intern') {
-            employees += `
-            <div class="card employee-card border-primary shadow">
-                <div class="card-header text-center text-white bg-primary">
-                    <h2 class="card-title">${team[i].name}</h2>
-                    <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${team[i].role}</h3>
-                </div>
-                <div class="card-body">
-                    <ul class="list-group">
-                        <li class="list-group-item">ID: ${team[i].id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
-                        <li class="list-group-item">School: ${team[i].school}</li>
-                    </ul>
-                </div>
-            </div>`
-        }
-    }
+            uniqueInfo = `<li class="list-group-item">School: ${team[i].school}</li>`
+        };
+        employees +=
+        `<div class="card employee-card border-primary shadow">
+            <div class="card-header text-center text-white bg-primary">
+                <h2 class="card-title">${team[i].name}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${team[i].role}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID: ${team[i].id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
+                    ${uniqueInfo}
+                </ul>
+            </div>
+        </div>`
+    }; 
     let site =  
 `<!DOCTYPE html>
 <html lang="en">
